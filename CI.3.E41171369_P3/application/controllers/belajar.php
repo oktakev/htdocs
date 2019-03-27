ef514a4a50479257904910bf6edb749f0722e63b
hasbi908 <48056689+hasbi908@users.noreply.github.com>
2019-03-27 19:24:37 +0700

    hasbi

--- CI.3.E41171369_P3/application/controllers/belajar.php
+++ CI.3.E41171369_P3/application/controllers/belajar.php
@@ -5,10 +5,11 @@ class Belajar extends CI_Controller {
 	
 	function __construct(){
 		parent::__construct();
-		$this->load->helper('html');
+		$this->load->model('m_data');
 	}
  
-	public function index(){		
-		$this->load->view('view_belajar');
+	public function user(){	
+		$data['user'] = $this->m_data->ambil_data()->result();	
+		$this->load->view('v_user.php',$data);
 	}
 }
